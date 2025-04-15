99b476a26d369e48517de257ae386ee46df18c9d
Yuliia <lupin773311asd@gmail.com>
2025-04-15 14:29:25 +0100

    амrh

--- src/components/MovieReviews.jsx
+++ src/components/MovieReviews.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getMovieReviews } from "../services/api"; // проверь путь
+import { getMovieReviews } from "../services/api"; 
 
 const MovieReviews = () => {
   const { movieId } = useParams();
