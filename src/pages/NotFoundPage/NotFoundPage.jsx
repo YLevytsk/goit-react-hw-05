151257628433468d0dad8e95759fb8d175453ac5
Yuliia <lupin773311asd@gmail.com>
2025-04-16 17:13:21 +0100

    jgk

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -3,19 +3,27 @@ import css from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
   return (
-    <div className={css.container}>
-      <h1 className={css.title}>404 - Page Not Found</h1>
-      <p className={css.text}>Sorry, the page you are looking for doesn’t exist.</p>
-      <Link to="/" className={css.link}>← Go to Home</Link>
+    <div className={css.section}>
+      <div className={css.container}>
+        <Link to="/" className={css.link}>← Go to Home</Link>
 
-      <img
-        src="/images/the-end-812226_1920.jpg"
-        alt="Movies banner"
-        className={css.banner}
-      />
+        <div className={css.bannerWrapper}>
+          <img
+            src="/images/the-end-812226_1920.jpg"
+            alt="Movies banner"
+            className={css.banner}
+          />
+          <div className={css.overlay}>
+            <h1 className={css.title}>404 - Page Not Found</h1>
+            <p className={css.text}>Sorry, the page you are looking for doesn’t exist.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default NotFoundPage;
+
+
 
